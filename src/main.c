6ff618cbb7734d1dd0c058855d1cd52b633d5c47
xavierbrasher <xavier.brasher@gmail.com>
2022-09-11 21:48:43 +0800

    Added more functions to the c cheatsheet

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h> 
 
-int mathFunctions() {
+int math_functions() {
 	// basic math functions 
 	printf("To the power to: %f, Square root of a num: %f, round-up: %f, round-down: %f \n", pow(2, 3), sqrt(36), ceil(34.4), floor(34.6));
 	return 0;
@@ -31,10 +31,91 @@ int comments(){
 	return 0;
 }
 
-int main() {
-	data_types_and_printf();
-	mathFunctions();
-	comments();
+int constants() {
+	const int NUM = 5; // or could be int const NUM = 5;
+	printf("Num: %d \n", NUM);
+	// num = 6; // This would error as its not mutable
+	printf("Num: %d \n", NUM);
+
+	printf("HELLO"); // considered a constant
+	// printf("&d", 90); // considered a constant
+
+	return 0;
+}
+
+int get_user_input() {
+	int age;
+	printf("Enter your age: ");
+	scanf("%d", &age);
+	printf("You are %d years old\n", age);
+
+	double gpa;
+	printf("Whats your GPA: ");
+	scanf("%lf", &gpa);
+	printf("Your GPA is %f\n", gpa);
 	
+	char name[100]; // worse alternative for getting char_array
+	printf("Whats your name: ");
+	scanf("%s", name);
+	printf("Your name is %s\n", name);
+	
+	getchar();
+	char full_name[100]; // better alternative for getting char_array
+	printf("Whats your full name: ");
+	fgets(full_name, 100, stdin);
+	printf("Your full name is %s\n", full_name);
+
+	return 0;
+}
+
+int basic_calculater() {
+	double num1;
+	double num2;
+	printf("Enter first number: ");
+	scanf("%lf", &num1);
+	printf("Enter second number: ");
+	scanf("%lf", &num2);
+	printf("Numbers added together equal too: %f\n", num1 + num2);
+
+	return 0;
+}
+
+int mad_libs_game() {
+	char color[20];
+	char plural_noun[20];
+	char celebrity_first[20];
+	char celebrity_last[20];
+
+	printf("Enter a color: ");
+	scanf("%s", color);
+
+	printf("Enter a plural noun: ");
+	scanf("%s", plural_noun);
+
+	printf("Enter a celebrity: ");
+	scanf("%s %s", celebrity_first, celebrity_last);
+
+	printf("Roses are %s\n", color);
+	printf("%s are blue\n", plural_noun);
+	printf("I love %s %s\n", celebrity_first, celebrity_last);
+
+	return 0;
+}
+
+int arrays() {
+	
+	return 0;
+}
+
+int main() {
+	// data_types_and_printf();
+	// math_functions();
+	// comments();
+	// constants();
+	// get_user_input();
+	// basic_calculater();
+	// mad_libs_game();
+	arrays();
+
 	return 0;
 }
